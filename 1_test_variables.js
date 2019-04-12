ebc076373aa4ab7b136a3868da3a77fbd800c8f8
persocom01 <persocom02@gmail.com>
2019-04-12 10:46:46 +0800

    0.02

---
+++ 1_test_variables.js
@@ -0,0 +1,11 @@
+// Javascript files are .js when independant of html files.
+/* Multiline comments
+are written this way. */
+
+// It is possible to declare a variable without a value.
+var testText = 'It\'s a test'
+var boo = true
+
+document.write(testText);
+
+alert(boo);
---
+++ README.md
@@ -0,0 +1,5 @@
+# TestJavaScript
+
+A JavaScript testing playground organized by topic in the sololearn tutorial.
+
+Workable code is written on each topic to demonstrate use of JavaScript in the topic area.
