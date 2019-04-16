0c6c0b957f9d077362ad82a4541dc2fa920f39f8
persocom01 <persocom02@gmail.com>
2019-04-16 09:35:37 +0800

    0.04

---
+++ 3_ifTest.js
@@ -0,0 +1,30 @@
+// Demonstrates if statements and logical operators.
+var input = 7
+// Demonstrates a conditional variable.
+// || = OR.
+var checkedInput = (input < -100 || input > 100) ? 'input is out of scope' : input
+var allowNegative = false
+
+// The types of typeof are:
+// 'number', 'string', 'boolean', 'undefined'.
+if (typeof checkedInput === 'string') {
+  console.log('the magnitude of x is greater than 100')
+// && = AND and ! = NOT
+} else if (checkedInput < 0 && !allowNegative) {
+  console.log('input cannot be negative')
+} else {
+// Adding a number to a strong concatenates both as a string.
+  console.log(input + ' passes the tests.')
+}
+
+switch (checkedInput) {
+  case 7:
+    console.log('you picked the lucky number seven!')
+    // break is neccessary or it will continue to the next case.
+    break
+  case 13:
+    console.log('you picked the unlucky number thirteen!')
+    break
+  default:
+    console.log('you picked an ordinary number.')
+}
---
+++ 4_loopsTest.js
@@ -0,0 +1,6 @@
+// Demonstrates loops.
+var array = ['mary', 'had', 'a', 'little', 'lamb']
+
+for (word of array) {
+
+}
