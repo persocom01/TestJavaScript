8648506eb15c39e13d1c667e67e6143aa1fc51d0
persocom01 <persocom02@gmail.com>
2019-04-23 10:40:55 +0800

    0.10

---
+++ 3_stringsTest.js
@@ -0,0 +1,9 @@
+// JS automatically converts numbers to strings in + operations.
+var x = 1 + ' punch man'
+console.log(x)
+
+// This is not true for other operations.
+x = '44' + 4
+console.log(x)
+x = '44' - 4
+console.log(x)
