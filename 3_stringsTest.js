01024dc8b300d0ba167db5302db9b164f2c1ea51
persocom01 <persocom02@gmail.com>
2019-04-23 10:48:45 +0800

    0.10

--- 3_stringsTest.js
+++ 3_stringsTest.js
@@ -3,7 +3,13 @@ var x = 1 + ' punch man'
 console.log(x)
 
 // This is not true for other operations.
-x = '44' + 4
+x = '11' + 1
 console.log(x)
-x = '44' - 4
+x = '11' - 1
+console.log(x)
+
+// Strings can be converted back to numbers using the + operator.
+x = '10' + '10'
+console.log(x)
+x = +'10' + +'10'
 console.log(x)
---
+++ 4_arrayTest.js
@@ -0,0 +1,4 @@
+// It is possible to define an array with an undefined element, unlike python.
+var arr = [1, , 3, 4, 5]
+
+console.log(arr)
