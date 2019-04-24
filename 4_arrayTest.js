3c67d7ebdef426dc8ef8fbe5b02ac15999889f45
persocom01 <persocom02@gmail.com>
2019-04-24 10:31:28 +0800

    0.11

--- 4_arrayTest.js
+++ 4_arrayTest.js
@@ -1,4 +1,5 @@
-// It is possible to define an array with an undefined element, unlike python.
-var arr = [1, , 3, 4, 5]
+// It is possible to define an array with an empty element, unlike python.
+// However, in practice, it is best to use undefined for empty elements.
+var arr = [1, , 3, undefined, 5]
 
 console.log(arr)
---
+++ 5_objectTest.js
@@ -0,0 +1,13 @@
+var request = 'tea'
+var menu = ['coffee', 'tea']
+var cafeObj = {type: 'Maid', getMenu: getMenu(request)}
+
+function getMenu(request) {
+  if (true) {
+
+  } else {
+
+  }
+}
+
+console.log(cafeObj.type)
