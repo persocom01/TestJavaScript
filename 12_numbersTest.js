3f28316157e71a469d236b85f49c167e531ad181
persocom01 <persocom02@gmail.com>
2019-05-07 09:14:33 +0800

    0.22

---
+++ 12_numbersTest.js
@@ -0,0 +1,10 @@
+// Demonstrates various functions related to math and numbers.
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.NaN)
+console.log(Number.NEGATIVE_INFINITY)
+console.log(Number.POSITIVE_INFINITY)
+console.log(Number.EPSILON)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.MAX_SAFE_INTEGER)
