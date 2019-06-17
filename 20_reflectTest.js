ca22066d56fd135f4e6c873bc5b7fe3b879589c6
persocom01 <persocom02@gmail.com>
2019-06-18 02:12:38 +0800

    0.41

--- 20_reflectTest.js
+++ 20_reflectTest.js
@@ -2,13 +2,12 @@
 // Reflect is generally an alternative to object methods and in some cases
 // returns more usable values compared to equivalent object methods.
 
-// Syntax is apply(function, this, argArray)
+// Syntax is apply(function, this, argArray).
 // Functions which work on their own without requring a this value need to be
 // passed one as undefined.
-console.log(Reflect.apply(Math.max, undefined, [1, 10, 5]))
-// You may also call apply using:
-// Math.max.apply(undefined, [1, 10, 5])
-console.log(Function.prototype.apply.call(Math.max, undefined, [1, 10, 5]))
+// You may also call apply using Math.max.apply(undefined, [1, 10, 5]).
+console.log('f-apply:', Function.prototype.apply.call(Math.max, undefined, [1, 10, 5]))
+console.log('r-apply:', Reflect.apply(Math.max, undefined, [1, 10, 5]))
 console.log()
 
 function Maid (name, attribute, bhw) {
@@ -20,9 +19,13 @@ var args = ['Mafuyu', 'sadistic']
 // Construct allows passing an array as if it were multiple arguments instead
 // of a single array. Alternatively, use new Maid(...args).
 var mafuyu = Reflect.construct(Maid, args)
-console.log(mafuyu.name)
+console.log('construct:', mafuyu.name)
+console.log()
 
-console.log(Reflect.defineProperty(mafuyu, 'role', { value: 'server' }))
+// Demonstrates the difference between the obejct and reflect version.
+console.log('o-defineProperty:', Object.defineProperty(mafuyu, 'role', { value: 'server' }))
+// Returns a boolean instead of the object.
+console.log('r-defineProperty:'Reflect.defineProperty(mafuyu, 'role', { value: 'server' }))
 console.log(mafuyu.role)
 
 // Reflect.deleteProperty()
