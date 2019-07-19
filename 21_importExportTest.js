50fd3d6d36bfa972be5e1ebc806c1f95fd410b69
persocom01 <persocom02@gmail.com>
2019-07-19 12:01:43 +0800

    0.53

--- 21_importExportTest.js
+++ 21_importExportTest.js
@@ -4,24 +4,37 @@
 // python.
 import * as mod1 from './modules/mod1.js'
 
-// Demonstrates renaming of imports and importing from a module aggregator.
+// Demonstrates the default import 'style' in JS. The imports are put inside
+// {} and you may optionally rename them with as. You may also import multiple
+// things by seperating them with a ,.
 // You are generally recommended to rename the objects on import instead
 // of when they are exported.
 import { Mod2Class as MyMod2Class, Mod3Class } from './myModuleAggregator.js'
 
 // Demonstrates a default import. Unlike named imports, no {} is needed.
-// You also have to name the default object immediately instead of using as.
+// You also have to name the module object immediately instead of optionally.
 // Default imports are not recommended by some users as it is difficult to see
 // what the imported object contains.
 import myMod1 from './myModule1.js'
+
+var squareBtn = document.querySelector('#button1')
+// A full list of possible events can be found at:
+// https://www.w3schools.com/jsref/dom_obj_event.asp
+// The addEventListener method takes in 3 arguments. The event, the function
+// to be executed when the event occurs, and a boolean value as to whether the
+// event is detected via capturing (true) or bubbling (false).
+// Bubbling is default, and it means the events of child elements take priority
+// over their parents.
+var m3c = new Mod3Class()
+squareBtn.addEventListener('click', () => {
+  m3c.mod3Fun()
+})
 
 document.querySelector('#import').textContent = mod1.mod1
 // When importing a class, you still need to assign it to a variable to use
 // its methods.
 var m2c = new MyMod2Class()
 m2c.mod2Fun()
-var m3c = new Mod3Class()
-m3c.mod3Fun()
 document.querySelector('#default1').textContent = myMod1.default1
 document.querySelector('#default2').textContent = myMod1.default2
 
--- myModule3.js
+++ myModule3.js
@@ -4,10 +4,14 @@ class Mod3Class {
   // Exported functions can use non exported variables.
   mod3Fun () {
     // .querySelectorAll selects all instead of just the first one.
-    // More css color codes can be found here:
-    // https://www.w3schools.com/cssref/css_colors.asp
-    document.querySelectorAll('div').style.color = '#6495ED'
-    document.querySelectorAll('div').bold()
+    // It returns an array so it needs to be iterated over.
+    let selection = document.querySelectorAll('.divText')
+    for (var i = 0; i < selection.length; i++) {
+      // More css color codes can be found here:
+      // https://www.w3schools.com/cssref/css_colors.asp
+      selection[i].style.color = '#6495ED'
+      selection[i].style.fontWeight = 'bold'
+    }
   }
 }
 
