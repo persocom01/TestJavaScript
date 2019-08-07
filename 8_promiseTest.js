2404e8e626a9d1656f0ad46149c917a62eee7d0a
persocom01 <persocom02@gmail.com>
2019-08-07 12:51:37 +0800

    0.58

--- 8_promiseTest.js
+++ 8_promiseTest.js
@@ -5,39 +5,49 @@
 // Gets url of image.
 var url = document.querySelector('#img1').src
 
-function getImage (url) {
+function get (url, response = 'blob') {
+  // It seems one of the most common use of promises is to download
+  // stuff with XMLHttpRequest.
+  // XMLHttpRequest's main advantage is the ability to update part of
+  // a webpage without reloading it. It is critical in AJAX programming.
+  var request = new XMLHttpRequest()
   // The promise constructor takes in a single argument; a callback with
   // two function as arguments; resolve for if the operation worked, reject if
   // it did not.
   return new Promise(function (resolve, reject) {
-    // It seems one of the most common use of promises is to download
-    // stuff with XMLHttpRequest.
-    // XMLHttpRequest's main advantage is the ability to update part of
-    // a webpage without reloading it. It is critical in AJAX programming.
-    var request = new XMLHttpRequest()
     request.open('GET', url)
     // In the case of an image, set responseType to blob for binary data.
     // Other types include document for html or xml and json for json.
-    request.responseType = 'blob'
+    request.responseType = response
     request.onload = function () {
       if (request.status === 200) {
+        // The promise returns anything inside the resolve function.
         resolve(request.response)
       } else {
-        reject(Error('Load unsuccessful, error code:' + request.statusText))
+        reject(alert('Load unsuccessful, error code:' + request.statusText))
       }
     }
     request.onerror = function () {
-      reject(Error('There was a network error.'))
+      reject(alert('There was a network error.'))
     }
     request.send()
   })
 }
 
-function dlImageButton () {
+function saveBlob (blob) {
   let a = document.createElement('a')
-  document.body.appendChild(a)
-  a.style = 'display:none'
-  a.href = url
+  a.href = window.URL.createObjectURL(blob)
   a.download = 'image'
   a.click()
+}
+
+function dlImageButton () {
+  // To use a promise object, one must attach .then(value => doSomething(value))
+  // to it. You may choose not to use an arrow function, but either way you
+  // need to use a new function and not immediately call an existing one.
+  // It is possible to string promises using a .then chain.
+  // To perform an action if the promise is not resolved, use .catch instead.
+  // In a promise chain, a single .catch at the end will execute no matter
+  // where in the chain the promise failed.
+  get(url).then(blob => saveBlob(blob))
 }
