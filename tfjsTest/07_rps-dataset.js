5dd32a3f7989f33bbf685e8f5936dc2286e63c9b
persocom01 <persocom02@gmail.com>
2020-04-28 23:58:30 +0800

    0.69

--- tfjsTest/07_rps-dataset.js
+++ tfjsTest/07_rps-dataset.js
@@ -1,34 +1,38 @@
 class RPSDataset {
-  constructor() {
+  constructor () {
     this.labels = []
   }
 
-  addExample(example, label) {
+  addExample (example, label) {
     if (this.xs == null) {
-      this.xs = tf.keep(example);
-      this.labels.push(label);
+      this.xs = tf.keep(example)
+      this.labels.push(label)
     } else {
-      const oldX = this.xs;
-      this.xs = tf.keep(oldX.concat(example, 0));
-      this.labels.push(label);
-      oldX.dispose();
+      const oldX = this.xs
+      this.xs = tf.keep(oldX.concat(example, 0))
+      this.labels.push(label)
+      oldX.dispose()
     }
   }
-  
-  encodeLabels(numClasses) {
+
+  encodeLabels (numClasses) {
     for (var i = 0; i < this.labels.length; i++) {
       if (this.ys == null) {
         this.ys = tf.keep(tf.tidy(
-            () => {return tf.oneHot(
-                tf.tensor1d([this.labels[i]]).toInt(), numClasses)}));
+          () => {
+            return tf.oneHot(
+              tf.tensor1d([this.labels[i]]).toInt(), numClasses)
+          }))
       } else {
         const y = tf.tidy(
-            () => {return tf.oneHot(
-                tf.tensor1d([this.labels[i]]).toInt(), numClasses)});
-        const oldY = this.ys;
-        this.ys = tf.keep(oldY.concat(y, 0));
-        oldY.dispose();
-        y.dispose();
+          () => {
+            return tf.oneHot(
+              tf.tensor1d([this.labels[i]]).toInt(), numClasses)
+          })
+        const oldY = this.ys
+        this.ys = tf.keep(oldY.concat(y, 0))
+        oldY.dispose()
+        y.dispose()
       }
     }
   }
--- tfjsTest/07_webcam.js
+++ tfjsTest/07_webcam.js
@@ -24,7 +24,7 @@ class Webcam {
    *     webcam feed.
    */
   constructor(webcamElement) {
-    this.webcamElement = webcamElement;
+    this.webcamElement = webcamElement
   }
 
   /**
@@ -33,24 +33,24 @@ class Webcam {
    */
 
 
-  capture() {
+  capture () {
     return tf.tidy(() => {
       // Reads the image as a Tensor from the webcam <video> element.
-      const webcamImage = tf.browser.fromPixels(this.webcamElement);
+      const webcamImage = tf.browser.fromPixels(this.webcamElement)
 
-      const reversedImage = webcamImage.reverse(1);
+      const reversedImage = webcamImage.reverse(1)
 
       // Crop the image so we're using the center square of the rectangular
       // webcam.
-      const croppedImage = this.cropImage(reversedImage);
+      const croppedImage = this.cropImage(reversedImage)
 
       // Expand the outer most dimension so we have a batch size of 1.
-      const batchedImage = croppedImage.expandDims(0);
+      const batchedImage = croppedImage.expandDims(0)
 
       // Normalize the image between -1 and 1. The image comes in between 0-255,
       // so we divide by 127 and subtract 1.
-      return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
-    });
+      return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1))
+    })
   }
 
   /**
@@ -59,13 +59,13 @@ class Webcam {
    */
 
 
-  cropImage(img) {
-    const size = Math.min(img.shape[0], img.shape[1]);
-    const centerHeight = img.shape[0] / 2;
-    const beginHeight = centerHeight - (size / 2);
-    const centerWidth = img.shape[1] / 2;
-    const beginWidth = centerWidth - (size / 2);
-    return img.slice([beginHeight, beginWidth, 0], [size, size, 3]);
+  cropImage (img) {
+    const size = Math.min(img.shape[0], img.shape[1])
+    const centerHeight = img.shape[0] / 2
+    const beginHeight = centerHeight - (size / 2)
+    const centerWidth = img.shape[1] / 2
+    const beginWidth = centerWidth - (size / 2)
+    return img.slice([beginHeight, beginWidth, 0], [size, size, 3])
   }
 
   /**
@@ -76,38 +76,38 @@ class Webcam {
    */
 
 
-  adjustVideoSize(width, height) {
-    const aspectRatio = width / height;
+  adjustVideoSize (width, height) {
+    const aspectRatio = width / height
     if (width >= height) {
-      this.webcamElement.width = aspectRatio * this.webcamElement.height;
+      this.webcamElement.width = aspectRatio * this.webcamElement.height
     } else if (width < height) {
-      this.webcamElement.height = this.webcamElement.width / aspectRatio;
+      this.webcamElement.height = this.webcamElement.width / aspectRatio
     }
   }
 
-  async setup() {
+  async setup () {
     return new Promise((resolve, reject) => {
       navigator.getUserMedia = navigator.getUserMedia ||
           navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
-          navigator.msGetUserMedia;
+          navigator.msGetUserMedia
       if (navigator.getUserMedia) {
         navigator.getUserMedia(
-            {video: {width: 224, height: 224}},
-            stream => {
-              this.webcamElement.srcObject = stream;
-              this.webcamElement.addEventListener('loadeddata', async () => {
-                this.adjustVideoSize(
-                    this.webcamElement.videoWidth,
-                    this.webcamElement.videoHeight);
-                resolve();
-              }, false);
-            },
-            error => {
-              reject(error);
-            });
+          { video: { width: 224, height: 224 } },
+          stream => {
+            this.webcamElement.srcObject = stream
+            this.webcamElement.addEventListener('loadeddata', async () => {
+              this.adjustVideoSize(
+                this.webcamElement.videoWidth,
+                this.webcamElement.videoHeight)
+              resolve()
+            }, false)
+          },
+          error => {
+            reject(error)
+          })
       } else {
-        reject();
+        reject()
       }
-    });
+    })
   }
 }
