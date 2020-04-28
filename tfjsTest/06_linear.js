318953c26c6e2335222ec3fd3ac9db9f92dc1fb2
Toh <clarenceboonteck.toh@dxc.com>
2020-04-28 08:02:21 +0800

    0.68

--- tfjsTest/06_linear.js
+++ tfjsTest/06_linear.js
@@ -1,1 +1,12 @@
 // Pre-trained models found at: https://github.com/tensorflow/tfjs-models
+
+async function run () {
+  const port = 90
+  const MODEL_URL = `http://127.0.0.1:${port}/model.json`
+  const model = await tf.loadLayersModel(MODEL_URL)
+  console.log(model.summary())
+  const input = tf.tensor2d([10.0], [1, 1])
+  const result = model.predict(input)
+  alert(result)
+}
+run()
