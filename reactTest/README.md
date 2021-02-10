eda086510cd06ea761e9261d6898f66f74f2ea70
persocom01 <persocom02@gmail.com>
2021-02-10 16:04:19 +0800

    0.96

--- reactTest/README.md
+++ reactTest/README.md
@@ -12,5 +12,50 @@ npx create-react-app react-app-name
 npm install jquery --save
 ```
 
-react does not accept class names with caps.
+npx installs a local copy of the app without having to install dependencies
+globally.
+react does not accept names with caps.
 --save makes jquery a dependency.
+
+## Usage
+
+In the react app folder, the `src` folder contains the files for the react components.
+
+### JSX
+
+JavaScript (JS) files in react are written in a language called JSX, which is a syntax extension to JS. It is basically a merger between html and JS such that one can use html tags in a JS file without passing it as a string. The things of note about JSX are as follows:
+
+1. Because class is a reserved keyword in JS, html tags in JSX use className instead.
+
+2. {var} can be used to insert code into JSX html blocks much like python f-strings.
+
+These two features are demonstrated below:
+
+```
+function helloUser(user) {
+  if (user) {
+    return (
+      <div className="App">
+        hello {user}
+      </div>
+    );
+  }
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+```
+
+
+
+```
+function App() {
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+```
--- reactTest/react-test/src/App.js
+++ reactTest/react-test/src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Arguments passed to react functions are always passed as objects. As such,
+// to avoid errors, you need to either iterate through the object keys or
+// reference specific object properties.
+// In addition react has a rule in that input properties to react components
+// cannot be modified. For example,
+// props.sum = props.sum + var
+// is not allowed.
+function TestLink(props) {
+  return (
+    // All react components may only have a single tag at the highest level.
+    // This means, for example, that you cannot return:
+    // <div></div>
+    // <div></div>
+    // If you don't want to use a parent tag, you can use:
+    // <React.Fragment></React.Fragment>
+    // instead, or its short hand:
+    // <></>
+    <div>
+      {/* Commenting within html tags in JSX must be done this way.*/}
+      <a href={props.href}>{props.text}</a>
+    </div>
+    <div>
+    </div>
+  )
+}
+
+function App(user) {
+  if (user.name) {
+    return (
+      <div className="App">
+        hello {user.name}
+        {/* The main function can reference other functions in the same file.*/}
+        <TestLink href="./test.html" text="test link"/>
+      </div>
+
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world
+      <TestLink href="./test.html" text="test link"/>
     </div>
   );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         hello world
+//         <TestLink href="./test.html" text="test link"/>
+//       </div>
+//     )
+//   }
+// }
+
 
 export default App;
