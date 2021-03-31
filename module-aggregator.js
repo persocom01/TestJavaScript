fad8ffec4b70943246235b7e5309caddcd54b1da
persocom01 <persocom02@gmail.com>
2021-04-01 05:01:10 +0800

    1.07

--- module-aggregator.js
+++ module-aggregator.js
@@ -6,6 +6,8 @@
 // due to the conditional nature of the code.
 
 // Demonstrated here with a class export, as putting all exports in a file into
-// a single object makes the syntax when exporting and importing easier.
+// a single object makes the syntax when exporting and importing easier. You
+// may also rename the exports using as, for instance:
+// export { default as mod1 } from './module1.js'
 export { Mod2Class } from './module2.js'
 export { Mod3Class } from './module3.js'
