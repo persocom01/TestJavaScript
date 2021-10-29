ed170c5a22b922d84ae6b0af6bc310cfe0c33a4a
persocom01 <persocom02@gmail.com>
2021-10-29 09:36:07 +0800

    1.25

--- 20a_Js-timing-test.js
+++ 20a_Js-timing-test.js
@@ -11,6 +11,8 @@
 
 // JS has no native sleep method. This is one way to get one.
 const timer = ms => new Promise(resolve => setTimeout(resolve, ms))
+const ms = 1000
+const minTime = 2000
 
 // console.time() and console.timeEnd() are native functions which can help time
 // functions. However, they do not return any value.
@@ -18,10 +20,13 @@ console.time('console time')
 // Using the difference between Date.now() allows you to get an execution time
 // that can be returned.
 var start = Date.now()
-timer(2000).then(() => {
+timer(1000).then(() => {
   var end = Date.now()
 
-  var timeSpent = (end - start) / 1000
-  console.log('time spent: ' + timeSpent)
-  console.timeEnd('console time')
+  var timeSpent = end - start
+  console.log('time spent (s): ' + (timeSpent / 1000))
+  if (timeSpent < minTime) {
+    const addTime = minTime - timeSpent
+    timer(addTime).then(() => console.timeEnd('console time'))
+  }
 })
